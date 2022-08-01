179ad3634411d5d98883760815d9eada9a6033f1
Amanda Portela <mandaptela@gmail.com>
2022-08-01 16:09:40 -0300

    Requisito 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import WalletForm from '../components/WalletForm';
 import PropTypes from 'prop-types';
+import WalletForm from '../components/WalletForm';
 // Feito com consulta ao https://serfrontend.com/blog/redux-com-react-para-iniciantes/index.html
 class Wallet extends React.Component {
   render() {
-    const { despesas } = this.props;
     return (
       <div className="carteira">
         <p>Lista de Gastos</p>
         <WalletForm />
-        {despesas.map((item, index) => <li key={ index }>{ item }</li>)}
       </div>
     );
   }
 }
-const mapStateToProps = (state) => ({
-  despesas: state.wallet.expenses,
-});
 
 Wallet.propTypes = {
   despesas: PropTypes.arrayOf(PropTypes.array).isRequired,
 };
-export default connect(mapStateToProps)(Wallet);
+export default Wallet;
